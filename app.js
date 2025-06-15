9c74c18f2eab99f07329d99a310165a8852016f2
gamehackerever <gamehackerever@gmail.com>
2025-06-15 13:32:28 +0530

    backend: Initialize backend base

---
+++ app.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const sequelize = require('./config/database');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+sequelize.sync().then(() => {
+  console.log('Database synced');
+}).catch(err => {
+  console.error('Failed to sync database:', err);
+});
+
+module.exports = app;
